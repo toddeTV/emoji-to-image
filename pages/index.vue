63cc87d408cc3cb29b1f278915f8bfbc3b22de3b
Thorsten Seyschab <business@todde.tv>
2023-12-26 23:17:07 +0100

    feat: added DOM element to PNG download example (#4)
    * feat: added dependency `html-to-image` and `downloadjs`
    
    * feat: added example for DOM element to image download
    
    * fix: typing
    
    * fix: removed unused imports

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,20 @@
-<template>Hello world!</template>
+<script setup lang="ts">
+import { toPng } from "html-to-image";
+import download from "downloadjs";
+
+async function divToPng() {
+  const domElement = document.getElementById("pngWrapper");
+  if (domElement === null) {
+    return;
+  }
+  const dataUrl = await toPng(domElement);
+  download(dataUrl, "my-node.png", "image/png");
+}
+</script>
+
+<template>
+  <div>
+    <div id="pngWrapper">Hello world!</div>
+    <button type="button" @click="divToPng">Generate & Download</button>
+  </div>
+</template>
