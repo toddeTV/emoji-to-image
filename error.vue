326e045f5f93ac512046cf886db79bac76556b14
Thorsten Seyschab <business@todde.tv>
2023-12-26 20:46:32 +0100

    feat: added basic global error page

---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  error: Object,
+});
+
+function handleError() {
+  clearError({ redirect: "/" });
+}
+
+const code = props.error?.statusCode ?? props.error?.code ?? 500;
+
+const message =
+  props.error?.statusMessage ?? props.error?.message ?? "Unknown.";
+</script>
+
+<template>
+  <div class="page justify-center items-center">
+    <h1>Error {{ code }}</h1>
+    <div class="mb-2">{{ message }}</div>
+    <!-- <pre>{{ props.error }}</pre> -->
+    <button @click="handleError()">Back home</button>
+  </div>
+</template>
