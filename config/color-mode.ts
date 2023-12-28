8367572fad33fae3f8001ee4fb668b1b4573dcce
Thorsten Seyschab <business@todde.tv>
2023-12-28 12:38:59 +0100

    feat: added color mode configuration

---
+++ config/color-mode.ts
@@ -0,0 +1,5 @@
+import type { NuxtConfig } from "@nuxt/schema";
+
+export const nuxtConfig: NuxtConfig["colorMode"] = {
+  preference: "system",
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { version } from "./package.json";
-import { nuxtConfig as tailwindcssNuxtConfig } from "./config/tailwind-css";
-import { nuxtConfig as uiNuxtConfig } from "./config/nuxt-ui";
+import { nuxtConfig as tailwindcssNuxtConfig } from "./config/tailwind-css.js";
+import { nuxtConfig as uiNuxtConfig } from "./config/nuxt-ui.js";
+import { nuxtConfig as colorModeNuxtConfig } from "./config/color-mode.js";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -23,6 +24,7 @@ export default defineNuxtConfig({
 
   tailwindcss: tailwindcssNuxtConfig,
   ui: uiNuxtConfig,
+  colorMode: colorModeNuxtConfig,
 
   devServer: {
     https: false,
