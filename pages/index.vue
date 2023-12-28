ffc3b591db62d22b57380bd34450883a504df791
Thorsten Seyschab <business@todde.tv>
2023-12-28 12:37:32 +0100

    feat: first implementation for emoji to image with user form input (#9)
    * feat: first implementation
    
    * feat: split emoji input and transform into printable representation
    
    * feat: scaled emoji with user input value
    
    * feat: added grid type selection
    
    * refactor: typed
    
    * chore: removed visible debug output

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,119 @@ import download from "downloadjs";
 import { z } from "zod";
 import type { FormSubmitEvent } from "#ui/types";
 
+const gridTypes = [
+  {
+    name: "force grid",
+    value: "even",
+  },
+  {
+    name: "fill only existing emojis",
+    value: "fill",
+  },
+];
+
 const schema = z.object({
   emoji: z.string().emoji(),
+  width: z.number().int().min(8).max(512),
+  height: z.number().int().min(8).max(512),
+  bgColor: z.string().min(4).max(9).regex(/^#/),
+  gridSize: z.number().int().min(1).max(16),
+  emojiScale: z.number().int().min(1).max(100),
+  gridType: z.union([z.literal("even"), z.literal("fill")]),
 });
 
-type Schema = z.output<typeof schema>;
+type SchemaType = z.output<typeof schema>;
 
-const state = reactive({
-  emoji: undefined,
+type EmojiImageType = EmojiRowType[];
+type EmojiRowType = EmojiType[];
+type EmojiType = string;
+
+type SchemaTransformedType = SchemaType & {
+  emojiImages: EmojiImageType[];
+  emojiContainerDimensionPx: number;
+  emojiFontSizePx: number;
+};
+
+const state = reactive<SchemaType>({
+  emoji: "😀😃😅😄",
+  width: 64,
+  height: 64,
+  bgColor: "#392580",
+  gridSize: 2,
+  emojiScale: 80,
+  gridType: "even",
 });
 
-async function onSubmit(event: FormSubmitEvent<Schema>) {
-  // Do something with data
-  console.log(event.data);
+const lastValidState = ref<SchemaTransformedType>(getValidState(state)!); //TODO do not force non undefined here
+
+watch(
+  () => state,
+  (newState, oldState) => {
+    //TODO add deep compare, maybe with lodash? (Not `if (newState !== oldState) { ... }`)
+    const validState = getValidState(newState);
+    if (validState !== undefined) {
+      lastValidState.value = validState;
+    }
+  },
+  { deep: true }
+);
+
+function getValidState(
+  newState: SchemaType
+): SchemaTransformedType | undefined {
+  if (schema.safeParse(newState).success) {
+    const emojiContainerDimensionPx = getEmojiContainerDimensionPx(
+      newState.width,
+      newState.height,
+      newState.gridSize
+    );
+    return {
+      ...newState,
+      emojiImages: splitEmojis(state.emoji, state.gridSize),
+      emojiContainerDimensionPx: emojiContainerDimensionPx,
+      emojiFontSizePx: getEmojiFontSizePx(emojiContainerDimensionPx),
+    };
+  }
+  return undefined;
 }
 
-async function divToPng() {
+function splitEmojis(
+  emoji: string,
+  gridSize: number
+): SchemaTransformedType["emojiImages"] {
+  const emojiArray: string[] = [...emoji]; //TODO this does not work if combined emojis are used (e.g. 🏳️‍🌈 or 👨‍👨‍👧‍👧)
+  const rows: EmojiRowType[] = Array.from(
+    { length: Math.ceil(emojiArray.length / gridSize) },
+    (_, index) => emojiArray.slice(index * gridSize, (index + 1) * gridSize)
+  );
+  const emojiImages: EmojiImageType[] = Array.from(
+    { length: Math.ceil(rows.length / gridSize) },
+    (_, index) => rows.slice(index * gridSize, (index + 1) * gridSize)
+  );
+  return emojiImages;
+}
+
+function getEmojiContainerDimensionPx(
+  width: number,
+  height: number,
+  gridSize: number
+): SchemaTransformedType["emojiContainerDimensionPx"] {
+  const useSmallerDimension = width > height ? height : width;
+  const size = Math.floor(useSmallerDimension / gridSize);
+  return size;
+}
+
+function getEmojiFontSizePx(
+  emojiContainerDimensionPx: number
+): SchemaTransformedType["emojiFontSizePx"] {
+  return Math.floor(emojiContainerDimensionPx * (state.emojiScale / 100));
+}
+
+async function onSubmit(event: FormSubmitEvent<SchemaType>) {
+  divToPngAndDownload();
+}
+
+async function divToPngAndDownload() {
   const domElement = document.getElementById("pngWrapper");
   if (domElement === null) {
     return;
@@ -30,19 +127,150 @@ async function divToPng() {
 </script>
 
 <template>
-  <div>
-    <ColorModeSwitch />
+  <div class="flex flex-col gap-4 m-4">
+    <UCard>
+      <ColorModeSwitch />
+    </UCard>
 
     <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-      <UFormGroup label="Emojis" name="emoji">
-        <UInput v-model="state.emoji" type="emoji" />
-      </UFormGroup>
+      <UCard>
+        <template #header> Input </template>
 
-      <UButton type="submit"> Submit </UButton>
+        <div class="flex flex-col gap-2">
+          <UFormGroup label="Emojis" name="emoji">
+            <UInput v-model="state.emoji" type="text" />
+          </UFormGroup>
+
+          <UFormGroup label="Dimensions">
+            <div class="flex gap-x-2">
+              <UFormGroup name="width">
+                <UInput v-model="state.width" class="w-28" type="number">
+                  <template #trailing>
+                    <span class="text-gray-500 dark:text-gray-400 text-xs"
+                      >px</span
+                    >
+                  </template>
+                </UInput>
+              </UFormGroup>
+
+              <UIcon name="i-ph-x-bold" class="mt-2.5" />
+
+              <UFormGroup name="height">
+                <UInput v-model="state.height" class="w-28" type="number">
+                  <template #trailing>
+                    <span class="text-gray-500 dark:text-gray-400 text-xs"
+                      >px</span
+                    >
+                  </template>
+                </UInput>
+              </UFormGroup>
+            </div>
+          </UFormGroup>
+
+          <UFormGroup label="Background Color" name="bgColor">
+            <UInput v-model="state.bgColor" type="color" />
+          </UFormGroup>
+
+          <UFormGroup label="Grid Size NxN" name="gridSize">
+            <UInput v-model="state.gridSize" type="number" />
+          </UFormGroup>
+
+          <UFormGroup label="Grid Type" name="gridType">
+            <USelect
+              v-model="state.gridType"
+              :options="gridTypes"
+              option-attribute="name"
+            />
+          </UFormGroup>
+
+          <UFormGroup label="Emoji Scale" name="emojiScale">
+            <UInput v-model="state.emojiScale" class="w-28" type="number">
+              <template #trailing>
+                <span class="text-gray-500 dark:text-gray-400 text-xs">%</span>
+              </template>
+            </UInput>
+          </UFormGroup>
+        </div>
+      </UCard>
+
+      <UCard>
+        <template #header> Preview </template>
+        <div class="flex gap-2">
+          <div
+            class="border w-fit h-fit"
+            v-for="(emojiImage, emojiImageIndex) in lastValidState.emojiImages"
+            :key="emojiImageIndex"
+          >
+            <div
+              :id="`pngWrapper${emojiImageIndex}`"
+              :style="`
+                width: ${lastValidState.width}px;
+                height: ${lastValidState.height}px;
+                background-color: ${lastValidState.bgColor};
+              `"
+              class="flex flex-col justify-evenly"
+            >
+              <!-- TODO here the inner div is redundant, refactor! -->
+              <div
+                v-if="lastValidState.gridType === 'fill'"
+                v-for="(row, rowIndex) in emojiImage"
+                :key="rowIndex"
+                class="flex flex-row justify-evenly"
+              >
+                <div
+                  v-for="(emoji, emojiIndex) in row"
+                  :key="emojiIndex"
+                  class="flex justify-center items-center"
+                  :style="`
+                    width: ${lastValidState.emojiContainerDimensionPx}px;
+                    height: ${lastValidState.emojiContainerDimensionPx}px;
+                    line-height: ${lastValidState.emojiFontSizePx}px;
+                    font-size: ${lastValidState.emojiFontSizePx}px;
+                  `"
+                >
+                  {{ emoji }}
+                </div>
+              </div>
+              <div
+                v-if="lastValidState.gridType === 'even'"
+                v-for="rowIndex in Array.from(
+                  { length: lastValidState.gridSize },
+                  (_, index) => index
+                )"
+                :key="rowIndex"
+                class="flex flex-row justify-evenly"
+              >
+                <div
+                  v-for="columnIndex in Array.from(
+                    { length: lastValidState.gridSize },
+                    (_, index) => index
+                  )"
+                  :key="columnIndex"
+                  class="flex justify-center items-center"
+                  :style="`
+                    width: ${lastValidState.emojiContainerDimensionPx}px;
+                    height: ${lastValidState.emojiContainerDimensionPx}px;
+                    line-height: ${lastValidState.emojiFontSizePx}px;
+                    font-size: ${lastValidState.emojiFontSizePx}px;
+                  `"
+                >
+                  {{
+                    emojiImage[rowIndex]
+                      ? emojiImage[rowIndex][columnIndex]
+                      : ""
+                  }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </UCard>
+
+      <UCard>
+        <UButton type="submit">
+          Download {{ lastValidState.width }}px x {{ lastValidState.height }}px
+        </UButton>
+      </UCard>
     </UForm>
-
-    <div id="pngWrapper">Hello world!</div>
-
-    <button type="button" @click="divToPng">Generate & Download</button>
   </div>
 </template>
